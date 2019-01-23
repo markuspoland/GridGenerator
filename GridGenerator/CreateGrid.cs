503c0bd62346f35a0768cf86b6d6003c1310ac3e
Marek <markuspoland@gmail.com>
2019-01-23 15:10:15 +0100

    Functionality 1

---
+++ GridGenerator/CreateGrid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GridGenerator
+{
+    class CreateGrid
+    {
+        Dictionary<string, string> grid = new Dictionary<string, string>();
+
+        public CreateGrid()
+        {
+
+        }
+
+        public Dictionary<string, string> NewGrid(List<TextBox> textBoxes)
+        {
+            
+
+            foreach (TextBox box in textBoxes)
+            {
+                grid.Add(box.Name, box.Text);
+            }
+
+            return grid;
+        }
+    }
+}
--- GridGenerator/MainWindow.xaml.cs
+++ GridGenerator/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace GridGenerator
 {
@@ -20,9 +22,58 @@ namespace GridGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string gridFile = "MyGrid.json";
+
         public MainWindow()
         {
             InitializeComponent();
+            
+
+            if (!File.Exists(gridFile))
+            {
+                theGrid.Visibility = Visibility.Hidden;
+
+                MessageBoxResult gridMessage = MessageBox.Show("You don't have a GRID set up. Please fill out the grid and save it before generating your password chain.");
+
+                if (gridMessage == MessageBoxResult.OK)
+                {
+                    theGrid.Visibility = Visibility.Visible;
+                }
+
+            } else
+            {
+                theGrid.Visibility = Visibility.Visible;
+            }
+
+                        
         }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+            IEnumerable<TextBox> collection = theGrid.Children.OfType<TextBox>();
+            List<TextBox> txtb = collection.ToList();
+
+            CreateGrid newGrid = new CreateGrid();
+
+            var gridValues = newGrid.NewGrid(txtb);
+
+            string jsonGrid = JsonConvert.SerializeObject(gridValues, Formatting.Indented);
+
+            if (!File.Exists(gridFile))
+            {
+                using (StreamWriter file = File.CreateText(@"MyGrid.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, gridValues);
+                }
+            }
+            
+
+            
+            
+        }
+
+        
     }
 }
